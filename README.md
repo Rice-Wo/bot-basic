8d69fa3dcad53147a8704fd6d3b6088379a75eb4
Rice-Wo <yeeho2006@gmail.com>
2023-04-03 10:47:46 +0800

    上傳至github

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # alpha
  
+用於為之後自己寫的機器人提供框架
+
+## 感謝
+
+- tklog使用自: https://github.com/xinlin-z/tklog    
+- pycord API使用自: https://github.com/Pycord-Development/pycord
--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import tkinter as tk
 from tkinter import ttk
 from tkinter import * 
 import json
+import os
 
-
+#定義json檔讀寫函式
 def readJson(file):	
 	with open(f"{file}.json", "r", encoding='utf-8') as a:
 		f = json.load(a)
@@ -13,48 +14,56 @@ def writeJson(file, item):
 	with open(f"{file}.json", "w+") as f:
 		f.write(json.dumps(item, ensure_ascii=False))
 
+
 # this is a function to get the user input from the text input box
 def getInputBoxValue():
 	userInput = button.get()
 	return userInput
 
-
 # this is the function called when the button is clicked
 def btnClickFunction():
 	if getInputBoxValue():
-		tokenJson = readJson('token')
+		tokenJson = readJson('config')
 		key = getInputBoxValue()
-		tokenJson['TOKEN'] = key
-		writeJson('token', tokenJson)
+		tokenJson['token'] = key
+		writeJson('config', tokenJson)
 		
 	else:
 		print('error')
 
-
-
-root = Tk()
+tokenInput = Tk()
 
 # This is the section of code which creates the main window
-root.geometry('460x310')
-root.configure(background='#F0F8FF')
-root.title('Tittle')
-
+tokenInput.geometry('460x310')
+tokenInput.title('Tittle')
 
 # This is the section of code which creates a text input box
-button=Entry(root)
+button=Entry(tokenInput)
 button.place(x=126, y=26)
 
-
 # This is the section of code which creates the a label
-Label(root, text='輸入TOKEN', bg='#F0F8FF', font=('arial', 12, 'normal')).place(x=36, y=26)
-
+Label(tokenInput, text='輸入TOKEN', bg='#F0F8FF', font=('arial', 12, 'normal')).place(x=36, y=26)
 
 # This is the section of code which creates a button
-Button(root, text='a', bg='#F0F8FF', font=('arial', 12, 'normal'), command=btnClickFunction).place(x=286, y=26)
-token = readJson('token')
+Button(tokenInput, text='a', bg='#F0F8FF', font=('arial', 12, 'normal'), command=btnClickFunction).place(x=286, y=26)
 
-if 'TOKEN' in token:
-	if token['TOKEN']:
-		print(token['TOKEN'])
+
+
+if os.path.exists("config.json"):    
+	pass
 else:
-	root.mainloop()
+    # 創建一個新的設定檔
+    config = {
+        'token': "",
+        'setting2': 'value2',
+        'setting3': 'value3'
+    }
+    with open("config.json", 'w') as f:
+        json.dump(config, f)
+
+
+config = readJson('config')
+if config['token']:
+	print(config['config'])
+else:
+	tokenInput.mainloop()
