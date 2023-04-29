7178f000e45222e17295c0ed2ba1321202e27d0a
Rice-Wo <yeeho2006@gmail.com>
2023-04-29 08:00:58 +0800

    refactor : 改為使用cmd作為互動裝置
    這是B計畫:(

--- main.py
+++ main.py
@@ -1,34 +1,82 @@
 import tkinter as tk
-import json
 import os
-from windows import App
-import sys
-import subprocess
+import logging
 from fun import readJson, writeJson
+import discord
+from windows import error_window
+
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s [%(levelname)s] : %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+
+
+
+bot = discord.Bot(status=discord.Status.do_not_disturb, intents = discord.Intents().all())
+
+
+if os.path.exists("config.json"):    
+		pass
+else:
+	# 創建一個新的設定檔
+	config = {
+		'token': "",
+		'setting2': 'value2',
+		'setting3': 'value3'
+	}
+	writeJson('config', config)
+config = readJson('config')
+
+
+@bot.event
+async def on_ready():
+  print(f"機器人 {bot.user} 已經上線了")
+
+
+@bot.command(name='測試用')
+async def _test(ctx):
+  await ctx.respond('成功')
 
 
 
 
+
+
+
+
+
+
+
+def error(reason):
+	logging.critical(reason)
+	root = tk.Tk()
+	app = error_window(root)
+	root.mainloop()
+
+def tokenInput():
+	T = input('請輸入token')
+	config = readJson('config')
+	config['token'] = T
+	writeJson('config', config)
+
+
+def run_bot():
+	try:
+		token = config['token']
+		bot.run(token)
+	except discord.errors.LoginFailure:
+		config['token'] = ""
+		writeJson('config', config)
+		error('token錯誤或過期，請重新輸入')
+	except Exception as e:
+		error(e)
 
 
 if __name__ == "__main__":
 
-	if os.path.exists("config.json"):    
-		pass
-	else:
-		# 創建一個新的設定檔
-		config = {
-			'token': "",
-			'setting2': 'value2',
-			'setting3': 'value3'
-		}
-		writeJson('config', config)
+	
 
 
 	config = readJson('config')
 	if config['token']:
-		pass
+		run_bot()
 	else:
-		root = tk.Tk()
-		app = App(root)
-		root.mainloop()
+		tokenInput()
+		run_bot()
--- windows.py
+++ windows.py
@@ -7,59 +7,6 @@ from fun import writeJson, readJson
 
 
 
-class App:
-    def __init__(self, root):
-        self.root = root
-        #setting title
-        root.title("輸入token")
-        #setting window size
-        width=300
-        height=150
-        screenwidth = root.winfo_screenwidth()
-        screenheight = root.winfo_screenheight()
-        alignstr = '%dx%d+%d+%d' % (width, height, (screenwidth - width) / 2, (screenheight - height) / 2)
-        root.geometry(alignstr)
-        root.resizable(width=False, height=False)
-
-        tokenLabel=tk.Label(root)
-        ft = tkFont.Font(family='Times',size=18)
-        tokenLabel["font"] = ft
-        tokenLabel["fg"] = "#333333"
-        tokenLabel["justify"] = "center"
-        tokenLabel["text"] = "label"
-        tokenLabel.place(x=110,y=10,width=70,height=25)
-
-        global tokenInput
-        tokenInput=tk.Entry(root)
-        tokenInput["borderwidth"] = "1px"
-        ft = tkFont.Font(family='Times',size=13)
-        tokenInput["font"] = ft
-        tokenInput["fg"] = "#333333"
-        tokenInput["justify"] = "center"
-        tokenInput["text"] = "Entry"
-        tokenInput.place(x=50,y=50,width=200,height=25)
-
-        startButton=tk.Button(root)
-        startButton["bg"] = "#f0f0f0"
-        ft = tkFont.Font(family='Times',size=10)
-        startButton["font"] = ft
-        startButton["fg"] = "#000000"
-        startButton["justify"] = "center"
-        startButton["text"] = "Button"
-        startButton.place(x=110,y=90,width=70,height=25)
-        startButton["command"] = self.startButton_command
-
-    def startButton_command(self):
-        config = readJson('config')
-        config['token'] = tokenInput.get()
-        writeJson('config', config)
-        if os.path.exists("bot.exe"):
-            subprocess.Popen("bot/bot.exe")
-        else:
-            subprocess.Popen(["python", "bot/bot.py"])
-        
-        self.root.after(3000, self.root.destroy)
-        sys.exit()
 
 
 
