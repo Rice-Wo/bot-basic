49af11f654f6a7c4ab2cd8297b5548ff77a48410
Rice-Wo <yeeho2006@gmail.com>
2023-05-19 17:59:26 +0800

    style/feat : 詳見敘述
    style : 曾加註解，更改程式順序
    feat : 新增日誌

--- main.py
+++ main.py
@@ -6,36 +6,47 @@ import discord
 from windows import error_window
 from pathlib import Path
 from discord.ext import commands
+from datetime import datetime
+
+
+#設定日誌
+logs_folder = "logs"
+os.makedirs(logs_folder, exist_ok=True)
+
+current_time = datetime.now()
+log_filename = f"error_{current_time.strftime('%Y%m%d_%H%M%S')}.log"
+log_path = os.path.join(logs_folder, log_filename)
+
+logging.basicConfig(
+	level=logging.DEBUG,
+	format='%(asctime)s [%(levelname)s] : %(message)s',
+	datefmt='%Y-%m-%d %H:%M:%S'
+)
 
 
 
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s [%(levelname)s] : %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 
+#discord 機器人
 intents = discord.Intents.default()
 intents.message_content = True
 
 bot = discord.Bot(status=discord.Status.do_not_disturb, intents = intents)
 
 
-if not os.path.exists("config.json"):    
-	# 創建一個新的設定檔
-	config = {
-		'token': "",
-		'setting2': 'value2',
-		'setting3': 'value3'
-	}
-	writeJson('config', config)
+
 
 @bot.event
 async def on_ready():
-  logging.info(f"機器人 {bot.user} 已經上線了")
+	logging.info(f"機器人 {bot.user} 已經上線了")
+	logging.info('請勿關閉本視窗以維持機器人上線狀態，可以最小化')
+	logging.info('如要關閉機器人請使用/close 或者在本視窗按下ctrl+C')
 
 
 @bot.command(name='close')
 @commands.is_owner()
 async def _close(ctx):
-  await ctx.response('正在關閉機器人', el)
-  await bot.close()
+	await ctx.response('正在關閉機器人')
+	await bot.close()
 
 @_close.error
 async def on_application_command_error(ctx: discord.ApplicationContext, error: discord.DiscordException):
@@ -49,7 +60,16 @@ async def on_application_command_error(ctx: discord.ApplicationContext, error: d
 
 
 
-
+if not os.path.exists("config.json"):    
+	# 創建一個新的設定檔
+	config = {
+		'token': "",
+		'setting2': 'value2',
+		'setting3': 'value3'
+	}
+	writeJson('config', config)
+	config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "config.json")
+	logging.debug(f'已創建config.json，檔案位置 {config_file}')
 
 
 def error(reason):
@@ -82,10 +102,6 @@ for filepath in Path("./cogs").glob("**/*.py"):
 	bot.load_extension(f"cogs.{cog_name}")
 
 if __name__ == "__main__":
-
-	
-
-
 	config = readJson('config')
 	if config['token']:
 		run_bot()
