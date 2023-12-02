9e35b6a7486023bfa8c5cd1cdf1ff94e41b334ac
Rice-Wo <yeeho2006@gmail.com>
2023-12-02 20:02:46 +0800

    feat: 將指令列表加入預設cog

--- admin_cog.py
+++ admin_cog.py
@@ -23,7 +23,14 @@ class system(commands.Cog):
     async def _close(self, ctx):
         await ctx.respond('正在關閉機器人')
         await self.bot.close()
-        
+    
+
+    @discord.slash_command(name='commands-list指令列表', description='展示所有指令')
+    async def commandlist(self, ctx):
+        value="\n".join([str(i+1)+". "+x.name for i,x in enumerate(self.bot.commands)])
+
+        embed=discord.Embed(title='指令列表', description=value)
+        await ctx.respond(embed=embed)
     
     
     async def cog_command_error(self, ctx: commands.Context, error: commands.CommandError):
